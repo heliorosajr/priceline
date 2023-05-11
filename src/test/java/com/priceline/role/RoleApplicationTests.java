fbbf6ce6ead161840228ea57837434c812fe353a
Hélio De Rosa Junior <heliorosajr@gmail.com>
2023-05-11 18:03:49 -0300

    Added initial configuration using h2 database for test purposes and initial spring boot classes

---
+++ src/test/java/com/priceline/role/RoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priceline.role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
