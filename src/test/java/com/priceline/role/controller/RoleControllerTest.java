822cba065c5a0705a271952a7624df75e9c13922
Hélio De Rosa Junior <heliorosajr@gmail.com>
2023-05-15 13:02:14 -0300

    Added more junit tests for role controller

--- src/test/java/com/priceline/role/controller/RoleControllerTest.java
+++ src/test/java/com/priceline/role/controller/RoleControllerTest.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,12 +17,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -38,6 +42,7 @@ import com.priceline.role.service.system.MessageService;
 import com.priceline.role.utils.TestUtils;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class RoleControllerTest {
 	
 	private final String BASE_API = "/api/v1/role";
@@ -80,6 +85,43 @@ public class RoleControllerTest {
 
         // assert
         assertEquals(actual, role);
+	}
+	
+	@Test
+	@DisplayName("Get role by nonexistent uid")
+    public void testGetRoleByNonexistentUid() throws Exception {
+		// create random id
+		String id = UUID.randomUUID().toString();
+
+		// create exception
+		String description = messageService.getMessage(MessageEnum.EXCEPTION_ENTITY_NOT_FOUND_DESCRIPTION, new Object[] {});
+		String errorMessage = messageService.getMessage(MessageEnum.EXCEPTION_ENTITY_NOT_FOUND_ERR, new Object[] { id });
+		String help = messageService.getMessage(MessageEnum.EXCEPTION_ENTITY_NOT_FOUND_HELP, new Object[] {});
+
+		PricelineApiException expectedException = new PricelineApiException(description, errorMessage, help, HttpStatus.NOT_FOUND);
+		
+		// create error
+		PricelineApiError error = new PricelineApiError(expectedException, null);
+		
+		// configure mock
+		when(roleService.findByUid(any())).thenThrow(expectedException);
+	    
+	    // send request
+	    MvcResult result = mockMvc.perform(
+	    			MockMvcRequestBuilders.get(BASE_API + "/" + id)
+	    			.contentType(MediaType.APPLICATION_JSON))
+//	           	.andDo(MockMvcResultHandlers.print()) // debug purposes only
+	    		.andExpect(status().isNotFound())
+	           	.andReturn();
+        
+        // convert JSON to object
+	    PricelineApiError actual = TestUtils.convertToObject(result.getResponse().getContentAsString(), PricelineApiError.class);
+
+        // assert
+        assertEquals(actual.getDescription(), error.getDescription());
+        assertEquals(actual.getErrorMessage(), error.getErrorMessage());
+        assertEquals(actual.getReason(), error.getReason());
+        assertEquals(actual.getStatus(), error.getStatus());
 	}
 	
 	@Test
@@ -167,6 +209,35 @@ public class RoleControllerTest {
 	}
 	
 	@Test
+	@DisplayName("Save role")
+    public void testSaveRole() throws Exception {
+		// create DTO
+		RoleDTO dto = TestUtils.createRoleDTO(false);
+		
+		// create role
+		Role expected = dto.toRole();
+		expected.setDefaultRole(true);
+		
+		// configure mock
+		when(roleService.save(dto)).thenReturn(expected);
+	    
+	    // send request
+	    MvcResult result = mockMvc.perform(
+	    			MockMvcRequestBuilders.post(BASE_API)
+	    			.contentType(MediaType.APPLICATION_JSON)
+	    			.content(TestUtils.asJsonString(dto)))
+//	           	.andDo(MockMvcResultHandlers.print()) // debug purposes only
+	    		.andExpect(status().isCreated())
+	           	.andReturn();
+        
+        // convert JSON to object
+	    Role actual = TestUtils.convertToObject(result.getResponse().getContentAsString(), Role.class);
+
+        // assert
+        assertEquals(actual, expected);
+	}
+	
+	@Test
 	@DisplayName("Save role without name")
     public void testSaveRoleWithoutName() throws Exception {
 		// create DTO
@@ -240,6 +311,61 @@ public class RoleControllerTest {
         assertEquals(actual.getStatus(), error.getStatus());
 	}
 
+	@Test
+	@DisplayName("Update role")
+    public void testUpdateRole() throws Exception {
+		// create DTO
+		RoleDTO dto = TestUtils.createRoleDTO(false);
+		dto.setUid(UUID.randomUUID().toString());
+		dto.setName("updated name");
+		
+		// create role
+		Role expected = dto.toRole();
+		
+		// configure mock
+		when(roleService.update(dto, dto.getUid())).thenReturn(expected);
+	    
+	    // send request
+	    MvcResult result = mockMvc.perform(
+	    			MockMvcRequestBuilders.put(BASE_API + "/" + dto.getUid())
+	    			.contentType(MediaType.APPLICATION_JSON)
+	    			.content(TestUtils.asJsonString(dto)))
+	           	.andDo(MockMvcResultHandlers.print()) // debug purposes only
+	           	.andReturn();
+        
+        // convert JSON to object
+	    Role actual = TestUtils.convertToObject(result.getResponse().getContentAsString(), Role.class);
+
+        // assert
+        assertEquals(actual, expected);
+	}
+
+	@Test
+	@DisplayName("Update default role")
+    public void testUpdateDefaultRole() throws Exception {
+		// create DTO
+		RoleDTO dto = TestUtils.createRoleDTO(false);
+		dto.setUid(UUID.randomUUID().toString());
+		
+		// create role
+		Role expected = dto.toRole();
+		
+		// configure mock
+		when(roleService.updateDefaultRole(dto.getUid())).thenReturn(expected);
+	    
+	    // send request
+	    MvcResult result = mockMvc.perform(
+	    			MockMvcRequestBuilders.put(BASE_API + "/set-default/" + dto.getUid())
+	    			.contentType(MediaType.APPLICATION_JSON))
+	           	.andDo(MockMvcResultHandlers.print()) // debug purposes only
+	           	.andReturn();
+        
+        // convert JSON to object
+	    Role actual = TestUtils.convertToObject(result.getResponse().getContentAsString(), Role.class);
+
+        // assert
+        assertEquals(actual, expected);
+	}
 	@Test
 	@DisplayName("Delete role")
     public void testDeleteRole() throws Exception {
